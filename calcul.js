8ae6dbc4dd18fd8fed9beb79538b15c9496da6d6
Cédric Milius <c.milius@adsoftware.fr>
2020-03-30 11:12:37 +0200

    all calcul test

--- calcul.js
+++ calcul.js
@@ -1,4 +1,19 @@
 const sum = (a, b) => {
     return a + b;
 }
-module.exports = sum;
+const mult = (a, b) => {
+    return a * b;
+}
+const div = (a, b) => {
+    return a / b;
+}
+const sub = (a, b) => {
+    return a - b;
+}
+
+module.exports = {
+    sum,
+    mult,
+    div,
+    sub
+};
--- calcul.test.js
+++ calcul.test.js
@@ -1,5 +1,17 @@
-const sum = require('./calcul');
+const {sum, mult, div, sub} = require('./calcul');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test('mult 2 x 2 to equal 4', () => {
+    expect(mult(2, 2)).toBe(4);
+});
+
+test('div 10 / 5 to equal 2', () => {
+    expect(div(10, 5)).toBe(2);
+});
+
+test('sub 3 - 2 to equal 0', () => {
+    expect(sub(3, 2)).toBe(1);
 });
