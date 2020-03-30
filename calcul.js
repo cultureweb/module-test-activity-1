0afb8257ce3b29d4d9d03994f32148e82a7189da
Cédric Milius <c.milius@adsoftware.fr>
2020-03-30 12:05:14 +0200

    test date 2000

--- calcul.js
+++ calcul.js
@@ -10,10 +10,19 @@ const div = (a, b) => {
 const sub = (a, b) => {
     return a - b;
 }
+const date2000 =  () => {
+    let entryDate = new Date("2000-01-01T00:00:00");
+    let now = Date.now();
+    console.log({entryDate})
+    console.log({now})
+
+    return ( now - entryDate ) /1000;
+};
 
 module.exports = {
     sum,
     mult,
     div,
-    sub
+    sub,
+    date2000
 };
--- calcul.test.js
+++ calcul.test.js
@@ -1,4 +1,4 @@
-const {sum, mult, div, sub} = require('./calcul');
+const {sum, mult, div, sub, date2000} = require('./calcul');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
@@ -14,4 +14,20 @@ test('div 10 / 5 to equal 2', () => {
 
 test('sub 3 - 2 to equal 0', () => {
     expect(sub(3, 2)).toBe(1);
+});
+
+describe('date2000', () => {
+    let originalDateNow = Date.now;
+
+    beforeEach(() => {
+        Date.now = () => new Date("2000-01-01T00:10:00").getTime()
+        });
+   
+    afterEach(() => {
+        Date.now = originalDateNow;
+    });
+
+    test('date2000', () => {
+        expect(date2000()).toBe(600);
+    });
 });
